96803383986b0c9b288b7eb66352e10fbf2a7f6f
Ben Gosney <bengosney@googlemail.com>
2024-07-07 21:14:50 +0100

    feat(bookings): command to find invalid bookins

---
+++ cerberus/management/commands/invalid_bookings.py
@@ -0,0 +1,19 @@
+# Django
+from django.core.exceptions import ValidationError
+from django.core.management.base import BaseCommand
+
+# Locals
+from ...models import Booking
+
+
+class Command(BaseCommand):
+    help = "Clean empty booking slots"
+
+    def handle(self, *args, **options):
+        self.stdout.write("Check for invalid bookings")
+
+        for booking in Booking.objects.all():
+            try:
+                booking.check_valid()
+            except ValidationError:
+                self.stdout.write(f"Found invalid booking: {booking.id}")

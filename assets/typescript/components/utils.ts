4fe9694927552bc2a91866bf0918c6576d0c552e
Ben Gosney <bengosney@googlemail.com>
2024-07-19 21:02:27 +0100

    fix(invoice): fix the adding/editing of invoice lines
    Fixes #964

--- assets/typescript/components/utils.ts
+++ assets/typescript/components/utils.ts
@@ -1,1 +1,16 @@
 export const pad = (n: number, len: number): string =>  n.toString().padStart(len, '0').slice(-len);
+
+export const removeFormPrefix = (element: Element, count: number) => {
+    element.querySelectorAll('*').forEach((element) => {
+        ['name', 'id'].forEach((attr) => {
+          const attr_value = element.getAttribute(attr);
+          if (attr_value !== null) {
+            const new_attr_value = attr_value.replace('__prefix__', `${count}`);
+            if (attr_value !== new_attr_value) {
+                console.log(attr_value, new_attr_value);
+                element.setAttribute(attr, new_attr_value);
+            }
+          }
+        });
+      });
+}

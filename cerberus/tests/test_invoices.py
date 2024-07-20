b1592f334b5e8af61a69bc7497e77b2d5f8d9746
Ben Gosney <bengosney@googlemail.com>
2024-07-20 08:41:47 +0100

    test(transitions): update test data

--- cerberus/tests/test_invoices.py
+++ cerberus/tests/test_invoices.py
@@ -115,8 +115,8 @@ def test_loaded_total(invoice):
 def test_available_state_transitions(invoice: Invoice):
     available_state_transitions = invoice.available_state_transitions
     expected_state_transitions = [
-        Transition(name="send", icon="icons/mail.svg", sort=0, url="invoice_send"),
-        Transition(name="void", icon="icons/invoice-void.svg", sort=0, url=""),
+        Transition(name="send", icon="icons/mail.svg", sort=0, meta={"url": "invoice_send", "icon": "icons/mail.svg"}),
+        Transition(name="void", icon="icons/invoice-void.svg", sort=0, meta={"icon": "icons/invoice-void.svg"}),
     ]
     assert available_state_transitions == expected_state_transitions
 
